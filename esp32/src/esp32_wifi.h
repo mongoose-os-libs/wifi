78bc47fe5a903622bcc1b31127043894a00664f6
Deomid Ryabkov <rojer@cesanta.com>
2017-07-24 08:07:42 +0100

    Factor out WiFi support into a lib
    PUBLISHED_FROM=0f833d162a7b1834246452d104d27d7e85e14fbe

---
+++ esp32/src/esp32_wifi.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2014-2017 Cesanta Software Limited
+ * All rights reserved
+ */
+
+#ifndef CS_MOS_LIBS_WIFI_ESP32_SRC_ESP32_WIFI_H_
+#define CS_MOS_LIBS_WIFI_ESP32_SRC_ESP32_WIFI_H_
+
+#include "esp_event.h"
+
+esp_err_t esp32_wifi_ev(system_event_t *event);
+
+#endif /* CS_MOS_LIBS_WIFI_ESP32_SRC_ESP32_WIFI_H_ */
