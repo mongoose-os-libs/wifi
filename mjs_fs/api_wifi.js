7cdbbf7b4de2235de6f5e6c3b4f26f3fcaa25043
Dmitry Frank <mail@dmitryfrank.com>
2017-11-16 11:26:00 +0200

    Improve docs for Wifi JS API
    PUBLISHED_FROM=d6f11b7043346faae64306483e79cc6355bbf6be

--- mjs_fs/api_wifi.js
+++ mjs_fs/api_wifi.js
@@ -8,7 +8,7 @@
 // {
 //   "ssid": "NetworkName",
 //   "bssid": "12:34:56:78:90:ab",
-//   "authMode": 2, // Auth mode, one of AUTH constants.
+//   "authMode": Wifi.AUTH_MODE_WPA_PSK, // Auth mode, one of AUTH constants.
 //   "channel": 11,
 //   "rssi": -70
 // }
@@ -21,6 +21,13 @@
 // ```
 
 // Must be kept in sync with enum mgos_wifi_auth_mode
+// ## **Auth modes**
+// - `Wifi.AUTH_MODE_OPEN`
+// - `Wifi.AUTH_MODE_WEP`
+// - `Wifi.AUTH_MODE_WPA_PSK`
+// - `Wifi.AUTH_MODE_WPA2_PSK`
+// - `Wifi.AUTH_MODE_WPA_WPA2_PSK`
+// - `Wifi.AUTH_MODE_WPA2_ENTERPRISE`
 Wifi.AUTH_MODE_OPEN = 0;
 Wifi.AUTH_MODE_WEP = 1;
 Wifi.AUTH_MODE_WPA_PSK = 2;
