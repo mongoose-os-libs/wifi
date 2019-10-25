dcfce0b7bbd4d8bc827752154e8e43d285e53255
Paul Needler <paul@reldene.co.uk>
2019-10-25 14:00:40 +0100

    changed to esp32_wifi_protocol_setup

--- src/esp32/esp32_wifi.c
+++ src/esp32/esp32_wifi.c
@@ -327,7 +327,7 @@ bool mgos_wifi_dev_sta_setup(const struct mgos_config_wifi_sta *cfg) {
     tcpip_adapter_dhcpc_start(TCPIP_ADAPTER_IF_STA);
   }
   const char *protocol = cfg->protocol;
-  r = mgos_wifi_protocol_setup(WIFI_IF_STA, protocol);
+  r = esp32_wifi_protocol_setup(WIFI_IF_STA, protocol);
   if (r != ESP_OK) {
     LOG(LL_ERROR, ("Failed to set STA protocol: %s", esp_err_to_name(r)));
     goto out;
@@ -489,7 +489,7 @@ bool mgos_wifi_dev_ap_setup(const struct mgos_config_wifi_ap *cfg) {
     goto out;
   }
   const char *protocol = cfg->protocol;
-  r = mgos_wifi_protocol_setup(WIFI_IF_AP, protocol);
+  r = esp32_wifi_protocol_setup(WIFI_IF_AP, protocol);
   if (r != ESP_OK) {
     LOG(LL_ERROR, ("Failed to set AP protocol: %s", esp_err_to_name(r)));
     goto out;
@@ -623,15 +623,13 @@ bool mgos_wifi_dev_start_scan(void) {
   return (r == ESP_OK);
 }
 
-esp_err_t mgos_wifi_protocol_setup(wifi_interface_t ifx, const char *prot) {
+esp_err_t esp32_wifi_protocol_setup(wifi_interface_t ifx, const char *prot) {
   uint8_t protocol = 0;
   esp_err_t r = ESP_OK;
-  LOG(LL_INFO, ("WiFi Setting %d protocol as %s", ifx, prot));
   if (strchr(prot, 'B') != NULL) protocol |= WIFI_PROTOCOL_11B;
   if (strchr(prot, 'G') != NULL) protocol |= WIFI_PROTOCOL_11G;
   if (strchr(prot, 'N') != NULL) protocol |= WIFI_PROTOCOL_11N;
-  if (strpbrk(prot, "LR") != NULL) protocol |= WIFI_PROTOCOL_LR;
-  LOG(LL_INFO, ("WiFi Protocol: %d", protocol));
-  r = esp_wifi_set_protocol(ifx, protocol);
-  return r;
+  if (strstr(prot, "LR") != NULL) protocol |= WIFI_PROTOCOL_LR;
+  LOG(LL_INFO, ("WiFi %s: protocol %s (%#x)", (ifx == WIFI_IF_STA ? "STA" : "AP"), prot, protocol));
+  return esp_wifi_set_protocol(ifx, protocol);
 }
