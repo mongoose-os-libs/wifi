4e4a2e6be434b85f11f031139d15af1264ea245f
needlerp <paul@reldene.co.uk>
2020-02-23 11:58:13 +0000

    ESP32: Added STA listen_interval
    Better validation of the protocol spec

--- src/esp32/esp32_wifi.c
+++ src/esp32/esp32_wifi.c
@@ -334,7 +334,10 @@ bool mgos_wifi_dev_sta_setup(const struct mgos_config_wifi_sta *cfg) {
       goto out;
     }
   }
-
+  if (cfg->listen_interval_ms > 0) {
+    LOG(LL_INFO, ("WiFi STA listen_interval: %dms", cfg->listen_interval_ms));
+    stacfg->listen_interval = cfg->listen_interval_ms / 100;
+  }
   r = esp_wifi_set_config(WIFI_IF_STA, &wcfg);
   if (r != ESP_OK) {
     LOG(LL_ERROR, ("Failed to set STA config: %d", r));
@@ -627,11 +630,23 @@ bool mgos_wifi_dev_start_scan(void) {
 
 esp_err_t esp32_wifi_protocol_setup(wifi_interface_t ifx, const char *prot) {
   uint8_t protocol = 0;
-  if (strchr(prot, 'B') != NULL) protocol |= WIFI_PROTOCOL_11B;
-  if (strchr(prot, 'G') != NULL) protocol |= WIFI_PROTOCOL_11G;
-  if (strchr(prot, 'N') != NULL) protocol |= WIFI_PROTOCOL_11N;
-  if (strstr(prot, "LR") != NULL) protocol |= WIFI_PROTOCOL_LR;
-  LOG(LL_INFO, ("WiFi %s: protocol %s (%#x)",
-                (ifx == WIFI_IF_STA ? "STA" : "AP"), prot, protocol));
+  if (strcmp(prot, "B") == 0) {
+    protocol = WIFI_PROTOCOL_11B;
+  } else if (strcmp(prot, "BG") == 0) {
+    protocol = (WIFI_PROTOCOL_11B | WIFI_PROTOCOL_11G);
+  } else if (strcmp(prot, "BGN") == 0) {
+    protocol = (WIFI_PROTOCOL_11B | WIFI_PROTOCOL_11G | WIFI_PROTOCOL_11N);
+  } else if (strcmp(prot, "LR") == 0) {
+    protocol = WIFI_PROTOCOL_LR;
+  } else if (strcmp(prot, "BLR") == 0) {
+    protocol = (WIFI_PROTOCOL_11B | WIFI_PROTOCOL_LR);
+  } else if (strcmp(prot, "BGLR") == 0) {
+    protocol = (WIFI_PROTOCOL_11B | WIFI_PROTOCOL_11G | WIFI_PROTOCOL_LR);
+  } else if (strcmp(prot, "BGNLR") == 0) {
+    protocol = (WIFI_PROTOCOL_11B | WIFI_PROTOCOL_11G | WIFI_PROTOCOL_11N | WIFI_PROTOCOL_LR);
+  } else {
+    return ESP_ERR_NOT_SUPPORTED;
+  }
+  LOG(LL_INFO, ("WiFi %s: protocol %s (%#x)", (ifx == WIFI_IF_STA ? "STA" : "AP"), prot, protocol));
   return esp_wifi_set_protocol(ifx, protocol);
 }
